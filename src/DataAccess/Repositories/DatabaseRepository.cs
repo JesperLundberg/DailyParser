0b7370d7265d5ee2aa7a589e323b68ef471bb041
Jesper Lundberg <lundberg.jesper@gmail.com>
2022-12-29 22:54:45 +0100

    Add more database operations

--- src/DataAccess/Repositories/DatabaseRepository.cs
+++ src/DataAccess/Repositories/DatabaseRepository.cs
@@ -22,4 +22,32 @@ public class DatabaseRepository : IDatabaseRepository
     {
         return await GameContext.Games.FindAsync(gameId);
     }
+
+    public async Task<IEnumerable<Game>> GetGamesByDateRangeAsync(DateOnly fromDate, DateOnly toDate)
+    {
+        return await GameContext.Games.Where(game => game.Started > fromDate && game.Finished < toDate)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Game>> GetGamesByFromDateAsync(DateOnly fromDate)
+    {
+        return await GameContext.Games.Where(game => game.Started > fromDate)
+            .ToListAsync();
+    }
+
+    public async Task<bool> CreateGameAsync(Game game)
+    {
+        await GameContext.AddAsync(game);
+        var changedRows = await GameContext.SaveChangesAsync();
+
+        return changedRows != 0;
+    }
+
+    public async Task<bool> CreateGamesAsync(IEnumerable<Game> games)
+    {
+        await GameContext.Games.AddRangeAsync(games);
+        var changedRows = await GameContext.SaveChangesAsync();
+
+        return changedRows != 0;
+    }
 }
--- src/DataAccess/Repositories/IDatabaseRepository.cs
+++ src/DataAccess/Repositories/IDatabaseRepository.cs
@@ -7,4 +7,12 @@ public interface IDatabaseRepository
     Task<IEnumerable<Game>> GetAllGamesAsync();
 
     Task<Game?> GetGameAsync(Guid gameId);
+
+    Task<IEnumerable<Game>> GetGamesByDateRangeAsync(DateOnly fromDate, DateOnly toDate);
+    
+    Task<IEnumerable<Game>> GetGamesByFromDateAsync(DateOnly fromDate);
+
+    Task<bool> CreateGameAsync(Game game);
+
+    Task<bool> CreateGamesAsync(IEnumerable<Game> games);
 }
