581a0b9635f75a03b6de2632ec88b99fab1a2aae
Jesper Lundberg <lundberg.jesper@gmail.com>
2022-12-29 22:56:31 +0100

    Add changes to parser service

--- src/Parser/Services/ParserService.cs
+++ src/Parser/Services/ParserService.cs
@@ -1,9 +1,20 @@
-﻿namespace Parser.Services;
+﻿using DataAccess.Repositories;
+
+namespace Parser.Services;
 
 public class ParserService
 {
-    public void ParseIntoDb()
+    private IDatabaseRepository DatabaseRepository { get; }
+
+    public ParserService(IDatabaseRepository databaseRepository)
     {
+        DatabaseRepository = databaseRepository;
+    }
+
+    public async Task<bool> ParseIntoDbAsync(string pathToFiles)
+    {
+        var files = await GetFileListAsync(pathToFiles);
         
+        return true;
     }
 }
